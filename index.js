0cca70d7ee8596222eda52df98962d41bf7232b8
Valentina Alegría <val.happy.m@gmail.com>
2019-05-23 20:06:36 -0500

    Servidor montado

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (request, response) {
+    response.render('home');
+});
+
+console.log("Servidor iniciado...");
+app.listen(3000);
